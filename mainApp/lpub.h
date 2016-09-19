ce1552fd4961056cd59bd35e947da6cedf7a290e
Trevor <trevor.sandy@gmail.com>
2016-09-19 03:19:10 +0000

    15-part-count-update-exclude-ign-files.patch

--- mainApp/lpub.h
+++ mainApp/lpub.h
@@ -480,6 +480,9 @@ public:
   {
     return ldrawFile.numSteps(modelName);
   }
+  int numParts(){
+    return ldrawFile.getPartCount();
+  }
   QString readLine(const Where &here);
 
   bool isSubmodel(const QString &modelName)
